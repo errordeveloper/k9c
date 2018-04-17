fb8a925bd772630eb8248f34d278135153c82053
Ilya Dmitrichenko <errordeveloper@gmail.com>
2018-04-17 12:50:46 +0100

    Few minor improvements

--- build/gcloud-git.sh
+++ build/gcloud-git.sh
@@ -3,9 +3,11 @@
 # TODO: add condition to disable this
 export PATH=/google-cloud-sdk/bin:$PATH
 
-gcloud config set core/disable_usage_reporting true
-gcloud config set component_manager/disable_update_check true
-gcloud config set metrics/environment github_docker_image
+(
+  gcloud config set core/disable_usage_reporting true
+  gcloud config set component_manager/disable_update_check true
+  gcloud config set metrics/environment github_docker_image
+) &> /dev/null
 
 git config --global credential.helper gcloud.sh
 git config --global user.email "dx+training@weave.works"
