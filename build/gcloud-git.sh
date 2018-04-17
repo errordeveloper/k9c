8784a6bd9cb251f90f02997375508a63c1dd4c8d
Ilya Dmitrichenko <errordeveloper@gmail.com>
2018-04-17 12:05:43 +0100

    Install gcloud without incidental dependencies

--- build/gcloud-git.sh
+++ build/gcloud-git.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env sh
 
-# add a way to disable the gcloud
+# TODO: add condition to disable this
 export PATH=/google-cloud-sdk/bin:$PATH
+
+gcloud config set core/disable_usage_reporting true
+gcloud config set component_manager/disable_update_check true
+gcloud config set metrics/environment github_docker_image
+
 git config --global credential.helper gcloud.sh
 git config --global user.email "dx+training@weave.works"
 git config --global user.name "k8s fan"
-
