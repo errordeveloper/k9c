92c1835d536c78314a5bdca563c2bc140e9d0ba5
Ilya Dmitrichenko <errordeveloper@gmail.com>
2018-04-18 15:47:05 +0100

    Always checkout master

--- build/clone-repos.sh
+++ build/clone-repos.sh
@@ -12,5 +12,5 @@ for repo in "${repos[@]}" ; do
   repo_url="$(echo "${repo}" | jq -r '.repo_url')"
   checkout_path="$(echo "${repo}" | jq -r '.checkout_path')"
   test -d "${checkout_path}" \
-    || git clone "${repo_url}" "${checkout_path}"
+    || git clone --branch master "${repo_url}" "${checkout_path}"
 done
