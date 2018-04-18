72a212ce7c8813e354e6bb26539698cba533cb18
Ilya Dmitrichenko <errordeveloper@gmail.com>
2018-04-18 11:53:18 +0100

    Update gitsrv, use json define workspace repos

---
+++ build/clone-repos.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -o errexit
+set -o pipefail
+set -o nounset
+
+repos=($(echo "${1}" | jq -c '.[]'))
+
+for repo in "${repos[@]}" ; do
+  repo_url="$(echo "${repo}" | jq -r '.repo_url')"
+  checkout_path="$(echo "${repo}" | jq -r '.checkout_path')"
+  test -d "${checkout_path}" \
+    || git clone "${repo_url}" "${checkout_path}"
+done
